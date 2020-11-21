2227825ad3a279224e1667c7b8f7fad31c80f0d9
tantangin <irsan00mansyur@gmail.com>
2020-11-22 00:45:49 +0800

    new rebisi

--- themes/public/rapid/components/header.php
+++ themes/public/rapid/components/header.php
@@ -12,24 +12,46 @@
     <nav class="main-nav float-right d-none d-lg-block">
       <ul>
         <li class="active"><a href="<?= base_url(); ?>">Beranda</a></li>
-        <li><a href="#about">Tentang Kami</a></li>
-        <li class="drop-down"><a href="#services">Video Saham</a>
-          <ul>
-            <li><a href="<?= base_url("video"); ?>">Video</a></li>
-            <li><a href="<?= base_url("kalkulator"); ?>">Kalkulator</a></li>
-          </ul>
-        </li>
-        <li><a href="#team">Tim</a></li>
-        <li><a href="#footer">Hubungi Kami</a></li>
-        <?php if (is_login()) : ?>
-          <li> <a href="<?= base_url("profile"); ?>"> <?= user()->name; ?></a></li>
-          <li>
-            <form action="auth/logout" method="post"><button type="submit" class="btn btn-danger p-2">Logout</button></form>
+        <?php if (current_url() != base_url()) : ?>
+          <li><a href="<?= base_url(); ?>#about">Tentang Kami</a></li>
+          <li class="drop-down"><a href="#services">Video Saham</a>
+            <ul>
+              <li><a href="<?= base_url("video"); ?>">Video</a></li>
+              <li><a href="<?= base_url("kalkulator"); ?>">Kalkulator</a></li>
+            </ul>
           </li>
-        <?php else : ?>
-          <li><a href="auth/login">Login</a></li>
-          <li><a href="/register/user">Register</a></li>
+          <li><a href="<?= base_url(); ?>#team">Tim</a></li>
+          <li><a href="#footer">Hubungi Kami</a></li>
+          <?php if (is_login()) : ?>
+            <li> <a href="<?= base_url("profile"); ?>"> <?= user()->name; ?></a></li>
+            <li>
+              <form action="auth/logout" method="post"><button type="submit" class="btn btn-danger p-2">Logout</button></form>
+            </li>
+          <?php else : ?>
+            <li><a href="auth/login">Login</a></li>
+            <li><a href="/register/user">Register</a></li>
+          <?php endif; ?>
+        <?php else :; ?>
+          <li><a href="#about">Tentang Kami</a></li>
+          <li class="drop-down"><a href="#services">Video Saham</a>
+            <ul>
+              <li><a href="<?= base_url("video"); ?>">Video</a></li>
+              <li><a href="<?= base_url("kalkulator"); ?>">Kalkulator</a></li>
+            </ul>
+          </li>
+          <li><a href="#team">Tim</a></li>
+          <li><a href="#footer">Hubungi Kami</a></li>
+          <?php if (is_login()) : ?>
+            <li> <a href="<?= base_url("profile"); ?>"> <?= user()->name; ?></a></li>
+            <li>
+              <form action="auth/logout" method="post"><button type="submit" class="btn btn-danger p-2">Logout</button></form>
+            </li>
+          <?php else : ?>
+            <li><a href="auth/login">Login</a></li>
+            <li><a href="/register/user">Register</a></li>
+          <?php endif; ?>
         <?php endif; ?>
+
       </ul>
     </nav><!-- .main-nav -->
 
